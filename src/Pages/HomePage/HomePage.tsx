335ae509073c60e4c183676980eccb17e38f6d9e
PraptiH <praptiantara1892@gmail.com>
2026-07-20 21:25:56 +0600

    Comment some line to check deployment

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,12 +1,12 @@
 import AiroSection from "../../Components/AiroSection/AiroSection"
 import Contact from "../../Components/Contact/Contact"
 import FeaturedProducts from "../../Components/FeaturedProducts/FeaturedProducts"
-import Footer from "../../Components/Footer/Footer"
+// import Footer from "../../Components/Footer/Footer"
 import Header from "../../Components/Header/Header"
 import HeroSection from "../../Components/HeroSection.tsx/HeroSection"
 import TemplateSection from "../../Components/TemplateSection/TemplateSection"
-import WebsiteMarketing from "../../Components/WebsiteMarketing/WebsiteMarketing"
-import WhyWork from "../../Components/WhyWork/WhyWork"
+// import WebsiteMarketing from "../../Components/WebsiteMarketing/WebsiteMarketing"
+// import WhyWork from "../../Components/WhyWork/WhyWork"
 
 
 function HomePage() {
